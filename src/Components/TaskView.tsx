01b38757af3687bff97d58e676af9cfcdfb549e7
skorpion <jmosinski@o2.pl>
2024-04-27 19:56:19 +0200

    test

--- src/Components/TaskView.tsx
+++ src/Components/TaskView.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+
+import styles from './TaskView.module.css'
 import { TaskResultProps } from '../Types'
 
 const TaskView: React.FC<TaskResultProps> = ({ content, toTaskView }) => {
 	return (
-		<div>
-			<h1>{content.task}</h1>
+		<div className={styles.box_results}>
+			<h3>{content.task}</h3>
 			<button onClick={() => toTaskView(content.id)}>usuń</button>
 		</div>
 	)
